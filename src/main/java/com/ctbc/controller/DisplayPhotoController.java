d4417bc6a657bd10f7c8875d3e1fab09674d4cd4
TizzyBac <TizzyBac TizzyBac@icloud.com>
2018-07-22 21:46:33 +0800

    完成JSP SHOW圖片 FROM DB

--- src/main/java/com/ctbc/controller/DisplayPhotoController.java
+++ src/main/java/com/ctbc/controller/DisplayPhotoController.java
@@ -1,19 +1,75 @@
 package com.ctbc.controller;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/DisplayPhotoController")
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import com.ctbc.model.dao.EmpPhotoDAO;
+import com.ctbc.model.vo.EmpPhotoVO;
+
+@WebServlet(value = "/display.do", name = "DisplayPhotoController")
 public class DisplayPhotoController extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
-       
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this, config.getServletContext());
+	}
+
+	@Autowired
+	private EmpPhotoDAO empPhotoDAO;
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String actionName = request.getParameter("action");
+		
+		switch (actionName) {
+			case "getPhotoById":
+				String photoId = request.getParameter("photoId");
+				try {
+					EmpPhotoVO empPhotoVO = empPhotoDAO.getEmpPhotoById(Integer.parseInt(photoId));
+					System.out.println(" >>> " + empPhotoVO);
+					
+					InputStream is = empPhotoVO.getPhotoFile();
+					try(BufferedInputStream bis = new BufferedInputStream(is);
+							BufferedOutputStream bos = new BufferedOutputStream(response.getOutputStream());) {
+						
+						int readed = 0;
+						byte[] bytes = new byte[2048 * 2];
+						while ( ( readed = bis.read(bytes)) != -1  ) {
+							System.out.println(" readed : " + readed + " kb.");
+							bos.write(bytes);
+						}
+						
+					} catch (Exception e) {
+						e.printStackTrace();
+					} finally {
+						if (is != null) {
+							is.close();
+						}
+					}
+				} catch (NumberFormatException | SQLException e) {
+					e.printStackTrace();
+				}
+				break;
+			default:
+				break;
+		}
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ src/main/webapp/test_getPhotoInJsp.jsp
@@ -0,0 +1,30 @@
+﻿<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+ 
+<!DOCTYPE html>
+<html>
+  <head>
+  	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta charset="UTF-8">
+    <title>測試JSP顯示圖片(From DB)</title>
+  </head>
+  <body>
+      <h1 style="color: blue;">>測試JSP顯示圖片(From DB)</h1>
+      
+      <c:url var="queryPhotoUrl" value="/display.do">
+      	  <c:param name="action" value="getPhotoById"/>
+          <c:param name="photoId" value="3"/>
+      </c:url>
+      
+      <c:url value="/display.do">
+      	  <c:param name="action" value="getPhotoById"/>
+          <c:param name="photoId" value="3"/>
+      </c:url>
+      
+      <hr>
+      
+      <hr/>
+      圖片：<img alt="" src='${queryPhotoUrl}'/>
+      
+  </body>
+</html>
